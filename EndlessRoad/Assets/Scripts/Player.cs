5f3c36ad42f5e8cdc91c8c44a780fe0c36c0b839
Benjamin <Paminjo@tutanota.com>
2022-03-08 12:28:08 +0100

    Player Animation added

--- EndlessRoad/Assets/Scripts/Player.cs
+++ EndlessRoad/Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float speedMovement= 3f;
+    Rigidbody2D rigidbody2D;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.A))
+        {
+            
+        }
     }
 }
