d3d15c842a8f5452f9f112b4164a2129122a352a
Benjamin <Paminjo@tutanota.com>
2022-03-11 12:39:46 +0100

    Reactivating of Objects added

--- EndlessRoad/Assets/Scripts/EndlessBackground.cs
+++ EndlessRoad/Assets/Scripts/EndlessBackground.cs
@@ -7,7 +7,9 @@ public class EndlessBackground : MonoBehaviour
     public BoxCollider2D collider2D;
     public Rigidbody2D rigidbody2D;
     
+    public GameObject InteractiveObjects;
 
+    Transform[] children;
     private float height;
     private float scrollSpeed = -2.5f;
     
@@ -17,6 +19,7 @@ public class EndlessBackground : MonoBehaviour
         height = collider2D.size.y;
         collider2D.enabled = false;
         rigidbody2D.velocity = new Vector2(0, scrollSpeed);
+        children = InteractiveObjects.GetComponentsInChildren<Transform>();
     }
 
     // Update is called once per frame
@@ -32,13 +35,19 @@ public class EndlessBackground : MonoBehaviour
     {
         Vector2 resetPosition = new Vector2(0, height * 3f);
         transform.position = (Vector2)transform.position + resetPosition;
-       
-        Transform[] children;
-        children = GetComponentsInChildren<Transform>();
         
-        foreach (Transform child in children)
+        foreach (var child in children)
         {
             child.gameObject.SetActive(true);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(true)
+        {
+            scrollSpeed -= 100;
         }
     }
 }
